e3d58e41db85ba60f58e12bc3012054645165715
Mohammed Terry-Jack <mohammedterryjack@gmail.com>
2020-07-24 00:45:40 +0100

    new files downloaded

--- raw_data/download_sefaria.py
+++ raw_data/download_sefaria.py
@@ -3,36 +3,55 @@ from os import system
 ############ INSTALLED IMPORTS ###########################
 ############   LOCAL IMPORTS   ###########################
 ##########################################################
-PATH_TO_SAVE = "data/tanakh/{cannon}/{book}_{language_code}.json"
-LANGUAGES = ("English","Hebrew")
-CANNON_BOOKS_MAP = {
-    "Torah":["Deuteronomy","Exodus","Genesis","Leviticus","Numbers"],
-    "Prophets":[
-        "Amos","Ezekiel","Habakkuk","Haggai","Hosea",
-        "I%20Kings","I%20Samuel","II%20Kings","II%20Samuel",
-        "Isaiah","Jeremiah","Joel","Jonah","Joshua",
-        "Judges","Malachi","Micah","Nahum","Obadiah",
-        "Zechariah","Zephaniah"
-    ],
-    "Writings":[
-        "Daniel","Ecclesiastes","Esther","Ezra",
-        "I%20Chronicles","II%20Chronicles","Job",
-        "Lamentations","Nehemiah","Proverbs",
-        "Psalms","Ruth","Song%20of%20Songs"
-    ]
-}
-URL = "https://raw.githubusercontent.com/Sefaria/Sefaria-Export/master/json/Tanakh/{cannon}/{book}/{language}/merged.json"
+def download_tanakh():
+    PATH_TO_SAVE = "tanakh/{cannon}/{book}_{language_code}.json"
+    LANGUAGES = ("English","Hebrew")
+    CANNON_BOOKS_MAP = {
+        "Torah":["Deuteronomy","Exodus","Genesis","Leviticus","Numbers"],
+        "Prophets":[
+            "Amos","Ezekiel","Habakkuk","Haggai","Hosea",
+            "I%20Kings","I%20Samuel","II%20Kings","II%20Samuel",
+            "Isaiah","Jeremiah","Joel","Jonah","Joshua",
+            "Judges","Malachi","Micah","Nahum","Obadiah",
+            "Zechariah","Zephaniah"
+        ],
+        "Writings":[
+            "Daniel","Ecclesiastes","Esther","Ezra",
+            "I%20Chronicles","II%20Chronicles","Job",
+            "Lamentations","Nehemiah","Proverbs",
+            "Psalms","Ruth","Song%20of%20Songs"
+        ]
+    }
+    URL = "https://raw.githubusercontent.com/Sefaria/Sefaria-Export/master/json/Tanakh/{cannon}/{book}/{language}/merged.json"
 
-for cannon,books in CANNON_BOOKS_MAP.items():
-    for book in books:
+    for cannon,books in CANNON_BOOKS_MAP.items():
+        for book in books:
+            for language in LANGUAGES:
+                url = URL.format(
+                    cannon=cannon,
+                    book=book,
+                    language=language
+                )
+                path_to_save = PATH_TO_SAVE.format(
+                    cannon=cannon.lower(),
+                    book=book.lower(),
+                    language_code = language[:2].lower()
+                )
+                system(f"curl {url} --output {path_to_save}")
+
+def download_kabbalah():
+    PATH_TO_SAVE = "kabbalah/{book}_{language_code}.json"
+    LANGUAGES = ("English","Hebrew")
+    BOOKS = ("Zohar","Sefer%20Yetzirah")
+    URL = "https://raw.githubusercontent.com/Sefaria/Sefaria-Export/master/json/Kabbalah/{book}/{language}/merged.json"
+
+    for book in BOOKS:
         for language in LANGUAGES:
             url = URL.format(
-                cannon=cannon,
                 book=book,
                 language=language
             )
             path_to_save = PATH_TO_SAVE.format(
-                cannon=cannon.lower(),
                 book=book.lower(),
                 language_code = language[:2].lower()
             )
